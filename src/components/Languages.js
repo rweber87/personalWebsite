fd0813702606af349c438707a8ecdee5a49d8393
Rob Weber <rob.weber87@gmail.com>
2017-08-14 21:38:12 -0400

    change color or progress bars for languages

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
-import { Grid, Row, Col, Image } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 function Languages() {
 	var ruby = "Ruby";
@@ -10,19 +10,22 @@ function Languages() {
 	var jq = "JQuery";
 	var html = "HTML";
 	var css = "CSS";
-	
+
 
 	return(
 		<Grid>
 			<Row className='about'>
 				<Col xs={18} md={12}>
-					<ProgressBar now={90} label={`${ruby}`} />
-					<ProgressBar now={90} label={`${js}`} />
-					<ProgressBar now={100} label={`${react}`} />
-					<ProgressBar now={75} label={`${sql}`} />
-					<ProgressBar now={65} label={`${jq}`} />
-					<ProgressBar now={70} label={`${html}`} />
-					<ProgressBar now={70} label={`${css}`} />
+					<h2 className="section-label pull-left" >Language Proficiency</h2>
+				</Col>
+				<Col xs={18} md={12}>
+					<ProgressBar bsClass="progress-bar ruby" now={90} label={`${ruby}`} />
+					<ProgressBar bsClass="progress-bar js" now={90} label={`${js}`} />
+					<ProgressBar bsClass="progress-bar react" now={100} label={`${react}`} />
+					<ProgressBar bsClass="progress-bar sql" now={75} label={`${sql}`} />
+					<ProgressBar bsClass="progress-bar jq" now={65} label={`${jq}`} />
+					<ProgressBar bsClass="progress-bar html" now={70} label={`${html}`} />
+					<ProgressBar bsClass="progress-bar css" now={70} label={`${css}`} />
 				</Col>
 			</Row>
 		</Grid>
